c4b265eb7bb6701c158f647d8c4c4aa607d18aa5
slimshadeey1 <slimshadeey1@gmail.com>
2014-07-20 18:45:32 -0700

    Cleaned up

--- src/java/mc/alk/arena/Bungee/BungeeCommand/CommandExec.java
+++ src/java/mc/alk/arena/Bungee/BungeeCommand/CommandExec.java
@@ -30,7 +30,8 @@ public class CommandExec {
         }
         switch (args.get(0)) {
             case "join":
-                player.performCommand(fullCommand); //For now I am passing these commands into the server as I will have the player on the server by then, its local.
+                player.sendMessage("Ben the system works :)");
+                //player.performCommand(fullCommand); //For now I am passing these commands into the server as I will have the player on the server by then, its local.
             case "leave":
                 player.performCommand(fullCommand);
             case "info": //TODO When we get hooks to all of the different methods in battle Arena's command system, We will be able to fill these with the necessary executions.
