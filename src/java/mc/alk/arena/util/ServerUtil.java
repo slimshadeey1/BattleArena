fc0027eb876d723ec398869c700c414504d6fa6b
slimshadeey1 <slimshadeey1@gmail.com>
2014-07-27 22:44:02 -0700

    Created easy to use command and channel API, this allows for plugin authors to easily register commands and etc. Bukkit/Server side coming soon.

--- src/java/mc/alk/arena/util/ServerUtil.java
+++ src/java/mc/alk/arena/util/ServerUtil.java
@@ -16,7 +16,9 @@ import java.util.UUID;
 public class ServerUtil {
 
     /**
+     * TODO I need to add bungee compatibility to this class.
      * The safe method to specify we want a player who is currently online
+     *
      * @param name Name of the player to find
      * @return Player or null
      */
@@ -34,7 +36,7 @@ public class ServerUtil {
             if (Defaults.DEBUG_VIRTUAL) {
                 return VirtualPlayers.getPlayer(id);
             }
-        } catch (Throwable e){
+        } catch (Throwable e) {
             /* do nothing, craftbukkit version is not great enough */
             Log.err("Craftbukkit version does not have find player by UUID yet.");
             Log.printStackTrace(e);
@@ -43,108 +45,117 @@ public class ServerUtil {
     }
 
     @Deprecated
-	public static Player findPlayerExact(String name) {
-		if (name == null)
-			return null;
-		Player player = Bukkit.getPlayerExact(name);
-		if (player != null)
-			return player;
-		if (Defaults.DEBUG_VIRTUAL){return VirtualPlayers.getPlayer(name);}
-		return null;
-	}
+    public static Player findPlayerExact(String name) {
+        if (name == null)
+            return null;
+        Player player = Bukkit.getPlayerExact(name);
+        if (player != null)
+            return player;
+        if (Defaults.DEBUG_VIRTUAL) {
+            return VirtualPlayers.getPlayer(name);
+        }
+        return null;
+    }
 
     @Deprecated
-	public static Player findPlayer(String name) {
-		if (name == null)
-			return null;
-		Player foundPlayer = Bukkit.getPlayer(name);
+    public static Player findPlayer(String name) {
+        if (name == null)
+            return null;
+        Player foundPlayer = Bukkit.getPlayer(name);
         if (foundPlayer != null) {
-            return foundPlayer;}
-        if (Defaults.DEBUG_VIRTUAL){foundPlayer = VirtualPlayers.getPlayer(name);}
+            return foundPlayer;
+        }
+        if (Defaults.DEBUG_VIRTUAL) {
+            foundPlayer = VirtualPlayers.getPlayer(name);
+        }
         if (foundPlayer != null) {
-            return foundPlayer;}
+            return foundPlayer;
+        }
         Player[] online = Bukkit.getOnlinePlayers();
-		if (Defaults.DEBUG_VIRTUAL){online = VirtualPlayers.getOnlinePlayers();}
+        if (Defaults.DEBUG_VIRTUAL) {
+            online = VirtualPlayers.getOnlinePlayers();
+        }
 
-		for (Player player : online) {
-			String playerName = player.getName();
+        for (Player player : online) {
+            String playerName = player.getName();
 
-			if (playerName.equalsIgnoreCase(name)) {
-				foundPlayer = player;
-				break;
-			}
-			if (playerName.toLowerCase().indexOf(name.toLowerCase(),0) != -1) {
-				if (foundPlayer != null) {
-					return null;}
+            if (playerName.equalsIgnoreCase(name)) {
+                foundPlayer = player;
+                break;
+            }
+            if (playerName.toLowerCase().indexOf(name.toLowerCase(), 0) != -1) {
+                if (foundPlayer != null) {
+                    return null;
+                }
 
-				foundPlayer = player;
-			}
-		}
+                foundPlayer = player;
+            }
+        }
 
-		return foundPlayer;
-	}
+        return foundPlayer;
+    }
 
     @Deprecated
-	public static OfflinePlayer findOfflinePlayer(String name) {
-		OfflinePlayer p = findPlayer(name);
-		if (p != null){
-			return p;
-		} else{
-			/// Iterate over the worlds to see if a player.dat file exists
-			for (World w : Bukkit.getWorlds()){
-				File f = new File(w.getName()+"/players/"+name+".dat");
-				if (f.exists()){
-					return Bukkit.getOfflinePlayer(name);
-				}
-			}
-			return null;
-		}
-	}
+    public static OfflinePlayer findOfflinePlayer(String name) {
+        OfflinePlayer p = findPlayer(name);
+        if (p != null) {
+            return p;
+        } else {
+            /// Iterate over the worlds to see if a player.dat file exists
+            for (World w : Bukkit.getWorlds()) {
+                File f = new File(w.getName() + "/players/" + name + ".dat");
+                if (f.exists()) {
+                    return Bukkit.getOfflinePlayer(name);
+                }
+            }
+            return null;
+        }
+    }
 
-	public static Player[] getOnlinePlayers() {
-		if (Defaults.DEBUG_VIRTUAL){
-			return VirtualPlayers.getOnlinePlayers();
-		} else {
-			return Bukkit.getOnlinePlayers();
-		}
-	}
+    public static Player[] getOnlinePlayers() {
+        if (Defaults.DEBUG_VIRTUAL) {
+            return VirtualPlayers.getOnlinePlayers();
+        } else {
+            return Bukkit.getOnlinePlayers();
+        }
+    }
 
-	public static void findOnlinePlayers(Set<String> names, Set<Player> foundplayers, Set<String> unfoundplayers) {
-		Player[] online = getOnlinePlayers();
-		for (String name : names){
-			Player lastPlayer = null;
-			for (Player player : online) {
-				String playerName = player.getName();
-				if (playerName.equalsIgnoreCase(name)) {
-					lastPlayer = player;
-					break;
-				}
+    public static void findOnlinePlayers(Set<String> names, Set<Player> foundplayers, Set<String> unfoundplayers) {
+        Player[] online = getOnlinePlayers();
+        for (String name : names) {
+            Player lastPlayer = null;
+            for (Player player : online) {
+                String playerName = player.getName();
+                if (playerName.equalsIgnoreCase(name)) {
+                    lastPlayer = player;
+                    break;
+                }
 
-				if (playerName.toLowerCase().indexOf(name.toLowerCase(),0) != -1) { /// many names match the one given
-					if (lastPlayer != null) {
-						lastPlayer = null;
-						break;
-					}
-					lastPlayer = player;
-				}
-			}
-			if (lastPlayer != null){
-				foundplayers.add(lastPlayer);
-			} else{
-				unfoundplayers.add(name);
-			}
-		}
-	}
+                if (playerName.toLowerCase().indexOf(name.toLowerCase(), 0) != -1) { /// many names match the one given
+                    if (lastPlayer != null) {
+                        lastPlayer = null;
+                        break;
+                    }
+                    lastPlayer = player;
+                }
+            }
+            if (lastPlayer != null) {
+                foundplayers.add(lastPlayer);
+            } else {
+                unfoundplayers.add(name);
+            }
+        }
+    }
 
-	public static Version getBukkitVersion(){
-		final String pkg = Bukkit.getServer().getClass().getPackage().getName();
-		String version = pkg.substring(pkg.lastIndexOf('.') + 1);
-		if (version.equalsIgnoreCase("craftbukkit")){
-			return new Version("v1_4_5-");
-		} else{
-			return new Version(version);
-		}
-	}
+    public static Version getBukkitVersion() {
+        final String pkg = Bukkit.getServer().getClass().getPackage().getName();
+        String version = pkg.substring(pkg.lastIndexOf('.') + 1);
+        if (version.equalsIgnoreCase("craftbukkit")) {
+            return new Version("v1_4_5-");
+        } else {
+            return new Version(version);
+        }
+    }
 
 
 }
